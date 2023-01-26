c9094f6e091c4c9d2c14f5ee833958ba59299665
Milo C <75094742+whotf1@users.noreply.github.com>
2023-01-25 18:22:45 -0600

    reverting to original readme generator, saving async for later, need to get this thing done on time

---
+++ main/createdReadMes/piojdfgopsdf.md
@@ -0,0 +1,18 @@
+
+# p-osdjmgf
+
+## posdnmf
+
+## undefined
+
+## pdknmf;skdnf
+
+
+
+
+
+
+--sectionscontent splitting thing
+
+undefined 
+    
--- main/js/script.js
+++ main/js/script.js
@@ -3,7 +3,6 @@ const FileSystem = require("fs")
 const inquirer = require("inquirer");
 
 
-
 const questions = [
   {
     type: 'input',
@@ -34,66 +33,79 @@ const questions = [
 ]
 
 
-function doThings(){
-  inquirer
-  .prompt(questions).then(response => {
-    let newFileName = `${response.title.toLowerCase().split(' ').join('')}`;
-    var arrOfSectionsContent
-    // response.sections.forEach(element => {
-    //   // arrOfSectionsContent.push(`## ${element} \n `);
+async function askTheQuestions(){
+  const results = await Promise.all(questions.map( question => {
+    const response = inquirer.prompt([question])
+    return response
+  }))
 
-    //   inquirer.prompt([{type:`input`,message:`add content to ${element} section`, name:`sections-${element}-content`}]).then(response => {
-    //     arrOfSectionsContent.push(`## ${element} \n -${response}`)
-    //   })
-    // });
-    promptsectionscontent(response);
-
-    createfile(newFileName, response, arrOfSectionsContent);
-  })};
-
-// maybe need async here
-  async function promptsectionscontent(response){
-
-    var arrOfSectionsContent = Promise.all(
-      response.sections.map(async element => {
-      // arrOfSectionsContent.push(`## ${element} \n `);
-        const getsectionscontent = [
-          {type:`input`,
-          message:`add content to ${element} section`, 
-          name:`sections-${element}-content`
-          }]
-      return await inquirer.prompt(getsectionscontent)
-    }))
-    console.log(arrOfSectionsContent);
-  }; 
-
-    // function(re-callable), takes filename and R as response from inquirer, and puts new readme into the createdReadMes folder
-  function createfile(newFileName, R, SectionsContent){
-    FileSystem.appendFile(`./createdReadMes/${newFileName}.md`,`
-# ${R.projName}
-
-## ${R.description}
-
-## ${R.linkedin}
-
-## ${R.github}
+  console.log(results)
+}
 
 
 
 
+// function doThings(){
+//   inquirer
+//   .prompt(questions).then(response => {
+//     let newFileName = `${response.title.toLowerCase().split(' ').join('')}`;
+//     var arrOfSectionsContent
+//     // response.sections.forEach(element => {
+//     //   // arrOfSectionsContent.push(`## ${element} \n `);
+
+//     //   inquirer.prompt([{type:`input`,message:`add content to ${element} section`, name:`sections-${element}-content`}]).then(response => {
+//     //     arrOfSectionsContent.push(`## ${element} \n -${response}`)
+//     //   })
+//     // });
+//     promptsectionscontent(response);
+
+//     createfile(newFileName, response, arrOfSectionsContent);
+//   })
+// };
+
+// // maybe need async here
+// async function promptsectionscontent(response){
+
+//   const arrOfSectionsContent = await Promise.all(
+//     response.sections.map(async element => {
+//     // arrOfSectionsContent.push(`## ${element} \n `);
+//       const getsectionscontent = [
+//         {type:`input`,
+//         message:`add content to ${element} section`, 
+//         name:`sections-${element}-content`
+//         }]
+//     return await inquirer.prompt(getsectionscontent)
+//   }))
+//   console.log(arrOfSectionsContent);
+// }; 
+
+//     // function(re-callable), takes filename and R as response from inquirer, and puts new readme into the createdReadMes folder
+//   function createfile(newFileName, R, SectionsContent){
+//     FileSystem.appendFile(`./createdReadMes/${newFileName}.md`,`
+// # ${R.projName}
+
+// ## ${R.description}
+
+// ## ${R.linkedin}
+
+// ## ${R.github}
+
+// ## Section Here
 
 
---sectionscontent splitting thing
 
-${SectionsContent.toString()} 
-    `, 
-      function (err) {
-      if (err) console.log(err);
-      console.log('Saved!');})
+
+// --sectionscontent splitting thing
+
+// ${SectionsContent} 
+//     `, 
+//       function (err) {
+//       if (err) console.log(err);
+//       console.log('Saved!');})
     
-  }
+//   }
 
-doThings();
+askTheQuestions();
 
 
 
